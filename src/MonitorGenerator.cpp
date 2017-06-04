b7656f9ff11b4a49439d883ccc41e759385b35aa
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-06-04 14:37:24 +0200

    Modified Advice/Pointcut Generation functions to return string vectors
    As in earlier development the generateAdvices and generatePointcuts functions of the MonitorGenerator class printed their output on the standard output, they have been correctly changed to properly return vectors of strings.
    In the meantime, the printing of quotation marks has been fixed in generatePointcuts.

--- src/MonitorGenerator.cpp
+++ src/MonitorGenerator.cpp
@@ -15,30 +15,33 @@ ag::MonitorGenerator::MonitorGenerator(
 std::vector<std::string> ag::MonitorGenerator::generateAdvices() {
   auto argZeroName = _arguments.front().name();
   auto argZeroType = _arguments.front().type();
-
+  std::vector<std::string> advice;
   // before advice
-  std::cout << "advice " << _functionName << "_exec(" << argZeroName
-            << ") : before(" << argZeroType << " " << argZeroName << ") {\n";
-  std::cout << "\tif (margot::foo::update(" << argZeroName << ")) {\n";
-  std::cout << "\t\tmargot::foo::manager.configuration_applied();\n";
-  std::cout << "\t}\n";
-  std::cout << "\tmargot::foo::start_monitor();";
-  std::cout << "}\n\n";
+  advice.push_back("advice " + _functionName + "_exec(" + argZeroName
+                  + ") : before(" + argZeroType + " " + argZeroName
+                  + ") {\n");
+  advice.push_back("\tif (margot::foo::update(" + argZeroName + ")) {\n");
+  advice.push_back("\t\tmargot::foo::manager.configuration_applied();\n");
+  advice.push_back("\t}\n");
+  advice.push_back("\tmargot::foo::start_monitor();");
+  advice.push_back("}\n\n");
   // after advice
-  std::cout << "advice " << _functionName << "_exec(" << argZeroName
-            << ") : after(" << argZeroType << " " << argZeroName << ") {\n";
-  std::cout << "\tmargot::foo::stop_monitor();\n";
-  std::cout << "\tmargot::foo::log();\n";
-  std::cout << "}\n\n";
+  advice.push_back("advice " + _functionName + "_exec(" + argZeroName
+            + ") : after(" + argZeroType + " " + argZeroName + ") {\n");
+  advice.push_back("\tmargot::foo::stop_monitor();\n");
+  advice.push_back("\tmargot::foo::log();\n");
+  advice.push_back("}\n\n");
+  return advice;
 }
+
 std::vector<std::string> MonitorGenerator::generatePointcuts() {
   auto argZeroName = _arguments.front().name();
   auto argZeroType = _arguments.front().type();
   auto ArgZeroReturnType = _arguments.front().type();
-
-  std::cout << "pointcut " << _functionName <<"_exec(" << argZeroType << " "
-            << argZeroName << ") = execution (" << _returnType << " " << " "
-            << _functionName << "(...) && args(" << argZeroName << ");" 
-
+  std::vector<std::string> pointcut;
+  pointcut.push_back("pointcut " + _functionName +"_exec(" + argZeroType + " "
+            + argZeroName + ") = execution(\"" + _returnType + " "
+            + _functionName + "(...)\") && args(" + argZeroName + ");");
+  return pointcut;
 }
 }
