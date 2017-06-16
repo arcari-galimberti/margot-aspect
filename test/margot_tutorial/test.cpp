97d4c41c484dfd7f72a3f81748b961cff4bdcffe
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-06-16 18:25:42 +0200

    Added function call, state condition and richer output
    A function called when the state changes has been added to the testing main program, and a state condition variable has been added to try the state change functionality.
    Self-explanatory labels have been added to the printed output, to easily identify what should have changed between one execution and the next one.

--- test/margot_tutorial/test.cpp
+++ test/margot_tutorial/test.cpp
@@ -1,18 +1,38 @@
 #include <chrono>
+#include <iostream>
 #include <margot.hpp>
 #include <thread>
 
-int condition = 1;
+const int num_goals = 3;
+const int change_goal_cycle = 4;
+const int change_state_cycle = 5;
+
+int goal_condition = 1;
+bool state_condition = true;
 
 void do_work(int trials) {
   std::this_thread::sleep_for(std::chrono::milliseconds(trials));
 }
 
+void mock_configure() { std::cout << "** Called function **\n"; }
+
 int main() {
   int trials = 100;
-  int repetitions = 10;
+  int repetitions = 20;
   for (int i = 0; i < repetitions; ++i) {
-    condition = (i % 3) + 1;
+    std::cout << "\nExecution #" << i + 1 << "\n";
+    if (i % change_goal_cycle == 0) {
+      if (goal_condition >= num_goals) {
+        goal_condition = 1;
+      } else {
+        goal_condition = goal_condition + 1;
+      }
+      std::cout << "** Changed my_execution_goal value **\n";
+    }
+    if (i % change_state_cycle == 0) {
+      state_condition = !state_condition;
+      std::cout << "** Changed state **\n";
+    }
     do_work(trials);
   }
 }
